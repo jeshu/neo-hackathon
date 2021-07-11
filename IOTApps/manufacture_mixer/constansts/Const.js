e356775562056165fd852779ba65ce2080ddcbbc
khchirag <khchirag.ext@deloitte.com>
2021-07-11 17:12:01 +0300

    added raw material app images

--- IOTApps/manufacture_mixer/constansts/Const.js
+++
@@ -1,8 +0,0 @@
-const scriptHash = '0xd887122a40fa042cd6343f07f1f98c4929117d59';
-const rpcAddress = 'http://localhost:50012';
-const userPvtKey =
-  '38921c670ecbf38d4889e5db306793754d23b53412a5fc7604c8f6c959798ec5';
-
-  module.exports  = {
-  scriptHash, rpcAddress, userPvtKey
-}
