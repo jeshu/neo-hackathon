1bbf42668ef9c0bb4823012fefc1a0de461bda60
khchirag <khchirag.ext@deloitte.com>
2021-07-11 18:07:48 +0300

    modofied Readme.md

--- clientApps/RawMaterialApp/src/constansts/Const.js
+++ clientApps/RawMaterialApp/src/constansts/Const.js
@@ -1,4 +1,4 @@
-export const scriptHash = '0xd887122a40fa042cd6343f07f1f98c4929117d59';
+export const scriptHash = '0x4d0bd0d7d3ca2803b92d61fb6e03409ed26add8d';
 export const rpcAddress = 'http://localhost:50012';
 export const userPvtKey =
   '38921c670ecbf38d4889e5db306793754d23b53412a5fc7604c8f6c959798ec5';
